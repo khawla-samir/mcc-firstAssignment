fbea1f81b6aaa577780148ef22ab4888dab092a6
khawla-samir <117123404+khawla-samir@users.noreply.github.com>
2023-03-02 18:59:35 +0200

    lots of changes

--- app/src/main/java/com/example/firstproj/MainActivity.kt
+++ app/src/main/java/com/example/firstproj/MainActivity.kt
@@ -1,11 +1,107 @@
 package com.example.firstproj
 
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.firstproj.Adabter.NoteRV
+import com.example.firstproj.Model.Note
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import java.util.EventListener
 
 class MainActivity : AppCompatActivity() {
+
+   lateinit var rv:RecyclerView
+    lateinit var add :ImageButton
+    lateinit var notes : ArrayList<Note>
+    private var db = Firebase.firestore
+
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        rv=findViewById(R.id.recyclerView)
+        rv.layoutManager=LinearLayoutManager(this)
+        add = findViewById(R.id.btnAdd)
+        notes = ArrayList()
+
+        var noteAdabter=NoteRV(this,notes)
+
+
+        add.setOnClickListener {
+            val i = Intent(this, MainActivity2::class.java)
+            startActivity(i)
+        }
+
+      //  db=FirebaseFirestore.getInstance()
+
+        db.collection("notes")
+            .get()
+            .addOnSuccessListener {
+                if(!it.isEmpty){
+                    for (data in it.documents){
+
+                        val note: Note?= data.toObject(Note::class.java)
+                            notes.add(note!!)
+
+
+                    }
+                    rv.adapter=noteAdabter
+                }
+               /* result ->
+                for (document in result) {
+                    Log.d("read", "${document.id} => ${document.data}")
+                }*/
+
+            }
+            .addOnFailureListener { exception ->
+                Log.w("not read", "Error getting documents.", exception)
+            }
+
+
+
+
+
+    /*
+       val db = Firebase.firestore
+
+       val subject = hashMapOf(
+           "name" to "MCC",
+           "num" to "MOPC 3312",
+           "hours" to 3
+       )
+
+       // Add a new document with a generated ID
+       db.collection("subjects")
+           .add(subject)
+           .addOnSuccessListener { documentReference ->
+               Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+           }
+           .addOnFailureListener { e ->
+               Log.w(TAG, "Error adding document", e)
+           }
+       db.collection("subjects")
+           .get()
+           .addOnSuccessListener { result ->
+               for (document in result) {
+                   Log.d("read", "${document.id} => ${document.data}")
+               }
+           }
+           .addOnFailureListener { exception ->
+               Log.w("not read", "Error getting documents.", exception)
+           }
+*/
     }
 }
--- app/src/main/java/com/example/firstproj/MainActivity2.kt
+++ app/src/main/java/com/example/firstproj/MainActivity2.kt
@@ -1,11 +1,66 @@
 package com.example.firstproj
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import org.w3c.dom.Text
 
 class MainActivity2 : AppCompatActivity() {
+    lateinit var name: EditText
+    lateinit var desc:EditText
+    lateinit var num:EditText
+    lateinit var add:Button
+    lateinit var show:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val db = Firebase.firestore
+        add = findViewById(R.id.add)
+        show = findViewById(R.id.show)
+        name=findViewById(R.id.nameTF)
+        desc=findViewById(R.id.descTF)
+        num=findViewById(R.id.numTF)
+
+        add.setOnClickListener {
+            if (name.text.isEmpty() || desc.text.isEmpty() || num.text.isEmpty()){
+                Toast.makeText(this, "Fill all fields", Toast.LENGTH_LONG).show()
+            }
+            else {
+                val note = hashMapOf(
+                    "name" to name.text.toString(),
+                    "desc" to desc.text.toString(),
+                    "num" to num.text.toString()
+                )
+
+                // Add a new document with a generated ID
+                db.collection("notes")
+                    .add(note)
+                    .addOnSuccessListener { documentReference ->
+                        Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+
+                        Toast.makeText(this, "Added successfully", Toast.LENGTH_LONG).show()
+                        name.hint=" "
+                        desc.hint=" "
+                        num.hint=" "
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w(TAG, "Error adding document", e)
+                    }
+            }
+        }
+        show.setOnClickListener {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
+
     }
 }
